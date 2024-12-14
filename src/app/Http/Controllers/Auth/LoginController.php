fccce7ea0997fb1a2588472c5c1850890edcf3cf
taisuke0804 <m83.matsuura.taisuke@gmail.com>
2024-12-14 12:31:39 +0900

    バリデーションメッセージを日本語に変更
    - ユーザーログイン処理の際のバリデーション
    - 言語ファイルの作成
    - php artisan lang:publish

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -41,5 +42,13 @@ class LoginController extends Controller
     public function showLoginForm()
     {
         return view('user-auth.login');
+    }
+
+    // ユーザーログインのバリデーションルールをオーバーライドしてカスタマイズ
+    public function validateLogin(Request $request){
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:8', 
+        ]);
     }
 }
