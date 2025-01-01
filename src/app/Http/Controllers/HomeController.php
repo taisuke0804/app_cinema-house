a067e3a7cf499db92fe64930c4ffc16d0b63c77d
taisuke0804 <m83.matsuura.taisuke@gmail.com>
2025-01-01 11:33:44 +0900

    ログインユーザー予約済み座席一覧出力

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Seat;
 
 class HomeController extends Controller
 {
@@ -17,13 +19,24 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * ユーザーの予約一覧を表示するページ.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        // return view('home');
-        return view('home.index');
+        $user = Auth::user();
+        $reservations = Seat::with([
+                'screening:id,start_time,end_time,movie_id',
+                'screening.movie:id,title'
+            ])
+            ->select('seats.id', 'seats.screening_id', 'seats.row', 'seats.number', 'seats.is_reserved')
+            ->join('screenings', 'seats.screening_id', '=', 'screenings.id') // screeningテーブルと結合
+            ->where('seats.user_id', $user->id)
+            ->where('seats.is_reserved', true)
+            ->orderBy('screenings.start_time', 'asc') // start_timeでソート
+            ->get();
+
+        return view('home.index')->with('reservations', $reservations);
     }
 }
