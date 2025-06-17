622960e821c0e53dd543e6fbc39f65ce633f963b
taisuke0804 <m83.matsuura.taisuke@gmail.com>
2025-06-17 21:07:53 +0900

    console.phpにスケジュール登録

--- src/routes/console.php
+++ src/routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\DeletePastScreenings;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('screenings:delete-past')->dailyAt('03:00');
